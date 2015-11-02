ba7ab120cc0d548525d2c020bb460ba5ea3a4d0c
Argenis Arriojas <argearriojas@gmail.com>
2015-11-02 00:40:21 -0430

    Adding docstrings

--- media.py
+++ media.py
@@ -1,3 +1,8 @@
+"""Media Library for Fresh Tomatoes
+
+This module contains the class Movie that is to be used by the Fresh Tomatoes
+Project.
+"""
 import webbrowser
 
 
@@ -7,15 +12,19 @@ class Movie():
     # Constructor
     def __init__(self, movie_title, movie_year, movie_storyline, poster_image,
         	     trailer_youtube, oscars_won=0):
-        # The Movie Title
+        """Constructor function
+        Initializes the attributes for the instance
+        inputs:
+            :movie_title: The Movie Title
+            :movie_year: The Year Released
+            :movie_storyline: The Movie Storyline
+            :poster_image: The URL to the Poster Image
+            :trailer_youtube: The Trailer Youtube URL
+            :oscars_won: Number of oscars the Movie won
+        """
         self.title = movie_title
-        # The Year Published
         self.year = movie_year
-        # The Movie Storyline
         self.storyline = movie_storyline
-        # The URL to the Poster Image
         self.poster_image_url = poster_image
-        # The Trailer Youtube URL
         self.trailer_youtube_url = trailer_youtube
-        # Number of oscars the Movie won
         self.oscars_won = oscars_won
